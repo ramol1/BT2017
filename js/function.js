9d7bc86a380ef240ed70fa0b04e378f2ab03be18
Ramol1 <luca.ramosbrito@students.bfh.ch>
2017-04-03 15:31:14 +0200

    changes to adjust medication

--- js/function.js
+++ js/function.js
@@ -1,1 +1,9 @@
 /* Custom JavaScript file. Do not use bootstrap JS files for changes. */
+
+
+/* Function to add more rows to existing medication table */
+
+function addRow()
+{
+    $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value=""></td><td><input type="text" name="schema" value=""></td><td><input type="text" name="unit" value=""></td><td><input type="text" name="applicationform" value=""></td><td><input type="text" name="fromdate" value=""></td><td><input type="text" name="todate" value=""></td><td><input type="text" name="instruction" value=""></td><td><input type="text" name="reason" value=""></td><td><input type="text" name="commentary" value=""></td></tr>');
+}
