bdfe745f06c697269d064345b9017885321cd924
Ramol1 <luca.ramosbrito@students.bfh.ch>
2017-04-04 11:33:06 +0200

    today's changes

--- js/function.js
+++ js/function.js
@@ -5,5 +5,5 @@
 
 function addRow()
 {
-    $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value=""></td><td><input type="text" name="schema" value=""></td><td><input type="text" name="unit" value=""></td><td><input type="text" name="applicationform" value=""></td><td><input type="text" name="fromdate" value=""></td><td><input type="text" name="todate" value=""></td><td><input type="text" name="instruction" value=""></td><td><input type="text" name="reason" value=""></td><td><input type="text" name="commentary" value=""></td></tr>');
+    $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value="" placeholder="Medikament"></td><td><input type="text" name="schema" value="" placeholder="Schema"></td><td><input type="text" name="unit" value="" placeholder="Einheit"></td><td><input type="text" name="applicationform" value="" placeholder="Art der Einnahme"></td><td><input type="text" name="fromdate" value="" placeholder="01.01.2017"></td><td><input type="text" name="todate" value="" placeholder="02.01.2017"></td><td><input type="text" name="instruction" value="" placeholder="Anwendungsinstruktion"></td><td><input type="text" name="reason" value="" placeholder="Einnahmegrund"></td><td><input type="text" name="commentary" value="" placeholder="Kommentar"></td></tr>');
 }
