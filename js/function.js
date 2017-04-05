96a34adc0f77532457c59c5309f775eace7c446e
Ramol1 <luca.ramosbrito@students.bfh.ch>
2017-04-05 15:07:44 +0200

    New features that don't work and updated jquery library

--- js/function.js
+++ js/function.js
@@ -2,8 +2,12 @@
 
 
 /* Function to add more rows to existing medication table */
-
 function addRow()
 {
     $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value="" placeholder="Medikament"></td><td><input type="text" name="schema" value="" placeholder="Schema"></td><td><input type="text" name="unit" value="" placeholder="Einheit"></td><td><input type="text" name="applicationform" value="" placeholder="Art der Einnahme"></td><td><input type="text" name="fromdate" value="" placeholder="01.01.2017"></td><td><input type="text" name="todate" value="" placeholder="02.01.2017"></td><td><input type="text" name="instruction" value="" placeholder="Anwendungsinstruktion"></td><td><input type="text" name="reason" value="" placeholder="Einnahmegrund"></td><td><input type="text" name="commentary" value="" placeholder="Kommentar"></td></tr>');
 }
+
+/* Function to open and collapse tables */
+$('.collapsed-table > caption').click(function() {
+   $(this).toggleClass('collapsed-table');
+})
