06d112fc2b2e7920ca59471d5cda58c05e0bd252
Ramol1 <luca.ramosbrito@students.bfh.ch>
2017-05-02 10:54:28 +0200

    some test files and the working function to iterate a table

--- js/function.js
+++ js/function.js
@@ -13,3 +13,30 @@ $(document).ready(function(){
         $("#panel").slideToggle("slow");
     });
 });
+
+
+/*Function to iterate content from a table and display as json*/
+var myRows = [];
+var headersText = [];
+var $headers = $("th");
+
+// Loop through grabbing everything
+var $rows = $("tbody tr").each(function(index) {
+  $cells = $(this).find("td");
+  myRows[index] = {};
+
+  $cells.each(function(cellIndex) {
+    // Set the header text
+    if(headersText[cellIndex] === undefined) {
+      headersText[cellIndex] = $($headers[cellIndex]).text();
+    }
+    // Update the row object with the header/cell combo
+    myRows[index][headersText[cellIndex]] = $(this).text();
+  });
+});
+
+// Let's put this in the object like you want and convert to JSON (Note: jQuery will also do this for you on the Ajax request)
+var myObj = {
+    "myrows": myRows
+};
+alert(JSON.stringify(myObj));
