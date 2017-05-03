e079ae70f459532e037f385829c4e835e76487eb
Ramol1 <luca.ramosbrito@students.bfh.ch>
2017-05-03 12:45:15 +0200

    it's somehow broken but still works

--- js/function.js
+++ js/function.js
@@ -1,15 +1,20 @@
 /* Custom JavaScript file. Do not use bootstrap JS files for changes. */
 
 
-/* Function to add more rows to existing medication table */
-function addRow()
-{
-    $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value="" placeholder="Medikament"></td><td><input type="text" name="schema" value="" placeholder="Schema"></td><td><input type="text" name="unit" value="" placeholder="Einheit"></td><td><input type="text" name="applicationform" value="" placeholder="Art der Einnahme"></td><td><input type="text" name="fromdate" value="" placeholder="01.01.2017"></td><td><input type="text" name="todate" value="" placeholder="02.01.2017"></td><td><input type="text" name="instruction" value="" placeholder="Anwendungsinstruktion"></td><td><input type="text" name="reason" value="" placeholder="Einnahmegrund"></td><td><input type="text" name="commentary" value="" placeholder="Kommentar"></td></tr>');
+// Function to add more rows to existing medication table
+function addRow(){
+  $('#myTable tr:last').after('<tr><td><input type="text" name="medicine" value="" placeholder="Medikament"></td><td><input type="text" name="activesubstance" value="" placeholder="Wirkstoff"></td><td><input type="text" name="code" value="" placeholder="Code"></td><td><input type="text" name="form" value="" placeholder="Galenische Form"></td><td><input type="text" name="dose" value="" placeholder="Dosis"></td><td><input type="text" name="unit" value="" placeholder="Einheit"></td><td><input type="text" name="schema" value="" placeholder="Schema"></td><td><input type="text" name="mode" value="" placeholder="Einnahmemodus"></td><td><input type="text" name="instruction" value="" placeholder="Anwendungsinstruktion"></td><td><input type="text" name="applicationform" value="" placeholder="Verabreichungsweg"></td><td><input type="text" name="reason" value="" placeholder="Anwendungsgrund"></td><td><input type="text" name="fromdate" value="" placeholder="01.01.2017"></td><td><input type="text" name="todate" value="" placeholder="02.01.2017"></td><td><input type="text" name="commentary" value="" placeholder="Kommentar"></td></tr>');
 }
 
-/* Function to open and collapse tables */
-$(document).ready(function(){
-    $("#flip").click(function(){
+// Function to remove rows from adjust medication table
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("myTable").deleteRow(i);
+}
+
+// Function to open and collapse tables
+$(document).ready(function() {
+    $("#flip").click(function() {
         $("#panel").slideToggle("slow");
     });
 });
@@ -22,17 +27,17 @@ var $headers = $("th");
 
 // Loop through grabbing everything
 var $rows = $("tbody tr").each(function(index) {
-  $cells = $(this).find("td");
-  myRows[index] = {};
+    $cells = $(this).find("td");
+    myRows[index] = {};
 
-  $cells.each(function(cellIndex) {
-    // Set the header text
-    if(headersText[cellIndex] === undefined) {
-      headersText[cellIndex] = $($headers[cellIndex]).text();
-    }
-    // Update the row object with the header/cell combo
-    myRows[index][headersText[cellIndex]] = $(this).text();
-  });
+    $cells.each(function(cellIndex) {
+        // Set the header text
+        if (headersText[cellIndex] === undefined) {
+            headersText[cellIndex] = $($headers[cellIndex]).text();
+        }
+        // Update the row object with the header/cell combo
+        myRows[index][headersText[cellIndex]] = $(this).text();
+    });
 });
 
 // Let's put this in the object like you want and convert to JSON (Note: jQuery will also do this for you on the Ajax request)
